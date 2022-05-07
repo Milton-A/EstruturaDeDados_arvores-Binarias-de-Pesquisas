a2e128d1e83f35f2be6b6287a87f3f5904dfcb9e
Milton-A <miltondantas33@outlook.com>
2022-05-08 00:43:35 +0100

    Segundo commit, criação do cabeçalho declaracao

--- AVL/declaracao.h
+++ AVL/declaracao.h
@@ -1,3 +1,6 @@
 typedef struct livro Livro;
 typedef struct avl Avl;
 void imprimir(Avl *arvore);
+int altura(Avl *arvore);
+Avl *inserirRaiz(Avl *arvore,Livro dados);
+void inserirfilhos(Avl *arvore,Livro dados);
