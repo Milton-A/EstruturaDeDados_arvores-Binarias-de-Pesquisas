1748d86d6d430c8f88052d51e13fa7e035707bab
Oivlisfriend <robertomarcelogongo@gmail.com>
2022-05-08 16:09:49 +0100

    adiconando novas mudanças

--- AVL/declaracao.h
+++ AVL/declaracao.h
@@ -3,8 +3,5 @@ typedef struct avl Avl;
 void imprimir(Avl *arvore);
 int altura(Avl *arvore);
 Avl *inserirRaiz(Avl *arvore,Livro dados);
-<<<<<<< HEAD
 Avl *inserirfilhos(Avl *arvore,Livro dados);
-=======
-void inserirfilhos(Avl *arvore,Livro dados);
->>>>>>> 081b12a96f139bd8ed9fad550f087c3941296838
+
